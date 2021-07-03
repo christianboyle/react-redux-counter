b3e385d798e71c45959c8288d6b357e9b7b0fb8a
Christian Boyle <mail@christianboyle.com>
2021-07-03 09:23:07 -0500

    🎉

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createStore } from "redux";
+import allReducers from "./redux/reducers";
+import { Provider } from "react-redux";
+
+//The created store
+const store = createStore(
+  allReducers,
+);
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
